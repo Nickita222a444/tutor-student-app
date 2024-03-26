0c30ee79031055bab560b4ed9bee54fa72534056
Nickita222a444 <nickitamalushev2016@yandex.ru>
2024-03-26 19:46:02 +0400

    Implemented the methods for handling the tutor's resume

--- backend/db.js
+++ backend/db.js
@@ -70,9 +70,118 @@ class Database {
       await mongoClient.close();
     }
   }
+
+  async isResumeExists(nickname) {
+    try {
+      await mongoClient.connect();
+      const db = mongoClient.db("tutor_db");
+      const resume = db.collection("resume");
+
+      if ((await resume.findOne({ nickname })) === null) return false;
+      return true;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await mongoClient.close();
+    }
+  }
+
+  async fillResume(
+    nickname,
+    full_name,
+    birth_date,
+    education,
+    about,
+    phone_number,
+    work_email,
+    qualification
+  ) {
+    try {
+      await mongoClient.connect();
+      const db = mongoClient.db("tutor_db");
+      const resume = db.collection("resume");
+
+      const maxResumeDoc = await resume
+        .find()
+        .sort({ resume_id: -1 })
+        .limit(1)
+        .toArray();
+
+      let curResumeId;
+      if (maxResumeDoc.length === 0) {
+        curResumeId = 1;
+      } else curResumeId = maxResumeDoc[0]["resume_id"] + 1;
+
+      console.log(curResumeId);
+
+      await resume.insertOne({
+        resume_id: curResumeId,
+        nickname,
+        full_name,
+        birth_date,
+        education,
+        about,
+        phone_number,
+        work_email,
+        qualification,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await mongoClient.close();
+    }
+  }
+
+  async updateResume(
+    nickname,
+    full_name,
+    birth_date,
+    education,
+    about,
+    phone_number,
+    work_email,
+    qualification
+  ) {
+    try {
+      await mongoClient.connect();
+      const db = mongoClient.db("tutor_db");
+      const resume = db.collection("resume");
+
+      await resume.findOneAndUpdate(
+        { nickname },
+        {
+          full_name,
+          birth_date,
+          education,
+          about,
+          phone_number,
+          work_email,
+          qualification,
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await mongoClient.close();
+    }
+  }
+
+  async showResume(nickname) {
+    try {
+      if ((await this.isResumeExists(nickname)) === false) return false;
+
+      await mongoClient.connect();
+      const db = mongoClient.db("tutor_db");
+      const resume = db.collection("resume");
+
+      return resume.findOne({ nickname });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await mongoClient.close();
+    }
+  }
 }
 
 const db = new Database();
 Object.freeze(db);
-
-db.checkUser("1", "3");
